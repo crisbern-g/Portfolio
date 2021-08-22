a44cd2c475bd539138e5982a10d9d9d965142790
Crisbern <crisberngozun022@gmail.com>
2021-08-22 23:24:47 +0800

    Added About me and Contace Me

--- Main/urls.py
+++ Main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view())
+    path('', views.HomeView.as_view(), name='home'),
+    path('about-me/', views.AboutMeView.as_view(), name='about_me'),
+    path('contact-me/', views.ContactMeView.as_view(), name='contact_me')
 ]
--- Main/views.py
+++ Main/views.py
@@ -5,3 +5,10 @@ from django.views import View
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'Main/index.html'
+
+
+class AboutMeView(TemplateView):
+    template_name = 'Main/about_me.html'
+
+class ContactMeView(TemplateView):
+    template_name = 'Main/contact_me.html'
