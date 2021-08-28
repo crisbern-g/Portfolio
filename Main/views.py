6d87d9484f497f6e8f36d410ab8c5b952ac04b78
Crisbern <crisberngozun022@gmail.com>
2021-08-28 23:12:35 +0800

    Made progress on Blogs

--- Main/views.py
+++ Main/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from django.views import View
+from Blog.models import BlogModel, TagsModel
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'Main/index.html'
+    
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['latest_blogs'] = BlogModel.objects.all().order_by('-blog_date_added')[:4]
+        context['tags'] =  TagsModel.objects.all().order_by('tag_name')
+
+        return context
 
 
 class AboutMeView(TemplateView):
